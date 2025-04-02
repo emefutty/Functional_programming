6ed625da0fce8350a7820669e8974c41e3648ba4
emefutty <ekrasnoslobodtseva@bk.ru>
2025-04-03 00:42:00 +0300

    5.3 Суперпозиция

--- Functions/Program.fs
+++ Functions/Program.fs
@@ -1,1 +1,22 @@
-﻿printfn "Hello World!"
+﻿open System
+
+let pi = 3.14159
+
+let areaOfCircle r = pi * r * r
+let volumeOfCylinder r h = areaOfCircle r * h
+
+[<EntryPoint>]
+let main argv =
+
+    let readFloat = Console.ReadLine >> float
+
+    printfn "Введите радиус круга:"
+    let r = readFloat()
+
+    printfn "Введите высоту цилиндра:"
+    let h = readFloat()
+
+    r |> areaOfCircle |> printfn "Площадь круга: %.2f"
+    r |> (fun r -> volumeOfCylinder r h) |> printfn "Объем цилиндра: %.2f"
+
+    0
