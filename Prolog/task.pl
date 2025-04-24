4767b3f05382d368e770ec7ee68eccb1465ba252
emefutty <ekrasnoslobodtseva@bk.ru>
2025-04-24 12:10:18 +0300

    lab8.3

--- Prolog/task.pl
+++ Prolog/task.pl
@@ -17,8 +17,19 @@ b_s(X):- b_s(X,Y), write(Y), nl, fail.
 
 
 %Задание 2
+
 father(X,Y) :- man(X), parent(X, Y).
 father(X) :- father(Y, X), write(Y), nl, fail.
 
 wife(X,Y) :- parent(X,Z), parent(Y,Z), woman(X), man(Y).
 wife(X) :- wife(Y, X), write(Y), nl, fail.
+
+%Задание 3
+
+grand_ma(X, Y) :- woman(X), parent(X, Z), parent(Z, Y).
+grand_mas(X):- grand_ma(Y,X), write(Y), nl, fail.
+
+grand_pa_and_da(X,Y):- grand_ma(Z,X), wife(Z,Y); grand_ma(Z,Y), wife(Z,X).
+
+niece(X, Y):- woman(X), parent(Z, X), b_s(Z, Y).
+nieces(X):- niece(Y, X), write(Y), nl, fail.
